312154931dfe01703ca96b51eeba61a1cda8f0c3
vambknight <vambknight@gmail.com>
2023-04-22 22:47:45 +0800

    feat: template commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,30 @@
 <template>
   <div class="hello">
+    <!--  变量使用双大括号  -->
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+
+    <!--  变量是html需要使用v-html进行绑定  -->
+    <div>{{ rawHtml }}</div>
+    <div v-html="rawHtml"></div>
+
+    <!--  动态变量使用v-bind进行绑定，可以简写  -->
+    <div v-bind:id="dynamicId">{{ dynamicId }}</div>
+    <div :id="dynamicId">{{ dynamicId }}</div>
+
+    <!--  v-if 会将不满足条件的元素从dom中删除掉  -->
+    <div v-if="flag1">{{ `flag1: ${flag1} True展示` }}</div>
+    <div v-else-if="flag2">{{ `flag2: ${flag2} True展示` }}</div>
+    <div v-else>{{ `flag1: ${flag1} False展示` }}</div>
+
+    <!--  v-show 使用 CSS display: none 将元素进行隐藏 -->
+    <div v-show="flag1">v-show true</div>
+    <div v-show="!flag1">v-show false</div>
+
+    <!--  v-for  -->
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="item in arrList" :key="item.key">
+        {{ item.name }}
+      </li>
     </ul>
   </div>
 </template>
@@ -33,13 +32,23 @@
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      msg: 'test message',
+      rawHtml: "<a href='http://www.baidu.com'>百度</a>",
+      dynamicId: 10001,
+      flag1: false,
+      flag2: false,
+      arrList: [
+        { id: 1, name: 'test1', key: 1 },
+        { id: 1, name: 'test2', key: 2 },
+        { id: 1, name: 'test3', key: 3 },
+      ]
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
